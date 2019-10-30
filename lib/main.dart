8e40930916f6868d29b7bd12e956145ba5c4f3ef
giovanni <giosave98@gmail.com>
2019-10-30 11:36:19 -0700

    Update final

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-
 import 'package:drawer/pages/draw.dart';
 import 'package:flutter/material.dart';
-void main()=>runApp(MaterialApp(
-  home: Draww(),
-  debugShowCheckedModeBanner: false,
-));
+
+  void main()=>runApp(MaterialApp(
+    home: Draww(),
+    debugShowCheckedModeBanner: false,
+  ));
  
--- lib/pages/draw.dart
+++ lib/pages/draw.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class Draww extends StatefulWidget {
@@ -6,38 +5,42 @@ class Draww extends StatefulWidget {
   _DrawwState createState() => _DrawwState();
 }
 
-class _DrawwState extends State<Draww> 
-{
- 
-  @override
-  Widget build(BuildContext context)
-   {
-    return Scaffold(
-    appBar: AppBar(title: Text('Aplicacion'),
-    actions: <Widget>[
-      IconButton(icon: Icon(Icons.search),
-      onPressed: (){},
-      )
-    ],
-    ), 
-    body: Center(child: Text('Drawer',style: TextStyle(fontSize: 45),),), 
-    drawer: Drawer(
-      child: ListView(
-        children: <Widget>[
-        _user('gio','gio@gmail.com',AssetImage('src/images/profile.png'),AssetImage('src/images/solo.png')),
-        _barra('Inicio',Icon(Icons.home,color: Colors.pinkAccent,)),
-       _barra('Vender',Icon(Icons.monetization_on,color: Colors.green,)),
-        _barra('Comprar',Icon(Icons.shop,color: Colors.black12,)),
-        _barra('Perfil',Icon(Icons.account_circle,color: Colors.blueAccent,)),
-         Divider(),
-        _barra('Configuracion',Icon(Icons.settings,color: Colors.grey,)),
-         _barra('Ayuda',Icon(Icons.settings,color: Colors.grey,)),
-        ],
-      ),
-    ),    
-    );
+class _DrawwState extends State<Draww> {
+    @override
+    Widget build(BuildContext context)
+    {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Aplicacion'),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                  onPressed: (){},
+              )
+            ],
+        ), 
+        body: Center(
+          child: Text('Drawer',
+            style: TextStyle(fontSize: 45),
+          ),
+        ), 
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              _user('gio','gio@gmail.com',AssetImage('src/images/profile.png'),AssetImage('src/images/solo.png')),
+              _barra('Inicio',Icon(Icons.home,color: Colors.pinkAccent,)),
+              _barra('Vender',Icon(Icons.monetization_on,color: Colors.green,)),
+              _barra('Comprar',Icon(Icons.shop,color: Colors.black12,)),
+              _barra('Perfil',Icon(Icons.account_circle,color: Colors.blueAccent,)),
+              Divider(),
+              _barra('Configuracion',Icon(Icons.settings,color: Colors.grey,)),
+              _barra('Ayuda',Icon(Icons.settings,color: Colors.grey,)),
+            ],
+          ),
+        ),    
+      );
+    }
   }
-}
 
   Widget _barra(String texto, Icon icono)
   {
@@ -47,35 +50,32 @@ class _DrawwState extends State<Draww>
       trailing: icono,
     );
   }
+
   Widget _user(String email, String usuario,AssetImage perfilPrincipal,AssetImage usuarioFondo)
   {
-     String networkImage='https://cdn.pixabay.com/photo/2012/08/27/14/19/evening-55067_960_720.png';
+    String networkImage='https://cdn.pixabay.com/photo/2012/08/27/14/19/evening-55067_960_720.png';
     return UserAccountsDrawerHeader( 
-
-            accountEmail: Text(email),
-            accountName: Text(usuario),
-            otherAccountsPictures: <Widget>[
-            GestureDetector(
-            onTap: (){},
-            child:  CircleAvatar( 
-                    backgroundImage: usuarioFondo,),)
-              ],
-            currentAccountPicture: GestureDetector(
-             onTap: (){},
-             child: CircleAvatar(
-                backgroundImage: perfilPrincipal,
-              ),),
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                image: NetworkImage(networkImage),
-              ),
-            ),
-          );
+      accountEmail: Text(email),
+      accountName: Text(usuario),
+      otherAccountsPictures: <Widget>[
+        GestureDetector(
+          onTap: (){},
+          child:  CircleAvatar( 
+            backgroundImage: usuarioFondo,
+          ),
+        )
+      ],
+      currentAccountPicture: GestureDetector(
+        onTap: (){},
+        child: CircleAvatar(
+          backgroundImage: perfilPrincipal,
+        ),
+      ),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+            image: NetworkImage(networkImage),
+        ),
+      ),
+    );
   }
-  /*ListTile(
-          title: Text('fisrt'),
-          trailing: Icon(Icons.home),
-          onTap: ()=>Navigator.of(context).push(MaterialPageRoute(
-            builder:(BuildContext context)=>Otras('primera'))),
-        ),*/
